921095db8aec13ad7b3a3b31c97607ba8804e759
benkillin <git.ben@b1towel.com>
2019-02-23 01:58:16 -0500

    added modifications since initial commit

--- iptables.sh
+++ iptables.sh
@@ -29,20 +29,39 @@ allowed_localhost="/root/firewall/allowed_localhost_ports.txt";
 # loopback interfaces are valid.
 /sbin/iptables -A INPUT -i lo -s $UNIVERSE -d $UNIVERSE -j ACCEPT
 /sbin/iptables -A INPUT -i lo -j ACCEPT
-/sbin/iptables -A OUTPUT -i lo -s $UNIVERSE -d $UNIVERSE -j ACCEPT
-/sbin/iptables -A OUTPUT -i lo -j ACCEPT
+/sbin/iptables -A OUTPUT -o lo -s $UNIVERSE -d $UNIVERSE -j ACCEPT
+/sbin/iptables -A OUTPUT -o lo -j ACCEPT
 
 ####################################################################
-# allow ICMP ping outbound
+# Allow docker
+#/sbin/iptables -A INPUT -i docker0 -j ACCEPT
+#/sbin/iptables -A INPUT -i docker0 --dport 443 -j ACCEPT
+#/sbin/iptables -A FORWARD -d 172.17.42.0/16 -o docker0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+#/sbin/iptables -A FORWARD -d 172.17.42.0/16 -o docker0 -j ACCEPT
+#/sbin/iptables -A FORWARD -s 172.17.42.0/16 -i docker0 -j ACCEPT
+#/sbin/iptables -A FORWARD -s 0.0.0.0/0 -i docker0 -j ACCEPT
+#/sbin/iptables -A OUTPUT -o docker0 -j ACCEPT
+
+
+
+
+####################################################################
+# allow ICMP
 /sbin/iptables -A OUTPUT -p icmp --icmp-type 8 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 /sbin/iptables -A INPUT -p icmp --icmp-type 0 -m state --state ESTABLISHED,RELATED -j ACCEPT
-# allow ICMP ping inbound
+
 /sbin/iptables -A INPUT -p icmp --icmp-type 8 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 /sbin/iptables -A OUTPUT -p icmp --icmp-type 0 -m state --state ESTABLISHED,RELATED -j ACCEPT
 
+#/sbin/iptables -A INPUT -p icmp -j ACCEPT
+#/sbin/iptables -A OUTPUT -p icmp -j ACCEPT
+#/sbin/iptables -A FORWARD -p icmp -j ACCEPT
+
+
+
 
 ##############################
-# Ban specified IP addresses 
+# Ban specified IP addresses
 awk '{ system("/sbin/iptables -A FORWARD -d " $1 " -j DROP"); \
        system("/sbin/iptables -A INPUT -d " $1 " -j DROP"); \
        system("/sbin/iptables -A OUTPUT -d " $1 " -j DROP"); \
@@ -59,7 +78,7 @@ awk '{ system("/sbin/iptables -A FORWARD -d " $1 " -j DROP"); \
 
 awk '{ print "/sbin/iptables -A OUTPUT -p " $1 " --sport 1024:65535 --dport " $2 " -m state --state NEW,ESTABLISHED -j ACCEPT"; \
        print "/sbin/iptables -A INPUT -p " $1 " --sport " $2 " --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT"; \
-	   print ""; \
+           print ""; \
        system("/sbin/iptables -A OUTPUT -p " $1 " --sport 1024:65535 --dport " $2 " -m state --state NEW,ESTABLISHED -j ACCEPT"); \
        system("/sbin/iptables -A INPUT -p " $1 " --sport " $2 " --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT"); }' $allowed_outbound;
 
@@ -69,7 +88,7 @@ awk '{ print "/sbin/iptables -A OUTPUT -p " $1 " --sport 1024:65535 --dport " $2
 
 awk '{ print "/sbin/iptables -A INPUT -p " $1 " --sport 1024:65535 --dport " $2 " -m state --state NEW,ESTABLISHED -j ACCEPT"; \
        print "/sbin/iptables -A OUTPUT -p " $1 " --sport " $2 " --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT"; \
-	   print ""; \
+           print ""; \
        system("/sbin/iptables -A INPUT -p " $1 " --sport 1024:65535 --dport " $2 " -m state --state NEW,ESTABLISHED -j ACCEPT"); \
        system("/sbin/iptables -A OUTPUT -p " $1 " --sport " $2 " --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT"); }' $allowed_inbound;
 
@@ -77,26 +96,30 @@ awk '{ print "/sbin/iptables -A INPUT -p " $1 " --sport 1024:65535 --dport " $2 
 # FORWARD
 awk '{ print "/sbin/iptables -A FORWARD -p " $1 " --sport 1024:65535 --dport " $2 " -m state --state NEW,ESTABLISHED -j ACCEPT"; \
        print "/sbin/iptables -A FORWARD -p " $1 " --sport " $2 " --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT"; \
-	   print ""; \
+           print ""; \
        system("/sbin/iptables -A FORWARD -p " $1 " --sport 1024:65535 --dport " $2 " -m state --state NEW,ESTABLISHED -j ACCEPT"); \
        system("/sbin/iptables -A FORWARD -p " $1 " --sport " $2 " --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT"); }' $allowed_forward;
 
 
 #####################
-# LOCALHOST ONLY 
-# we type out the loopback network here instead of using the variable because it got all jacked up 
-# mixing the bash vars with the awk vars and I didn't want to deal with it.
+# LOCALHOST ONLY
 awk '{ print "/sbin/iptables -A OUTPUT -p " $1 " -s 127.0.0.1/8 -d 127.0.0.1/8 --sport " $2 " -j ACCEPT"; \
        print "/sbin/iptables -A OUTPUT -p " $1 " -s 127.0.0.1/8 -d 127.0.0.1/8 --dport " $2 " -j ACCEPT"; \
        print "/sbin/iptables -A INPUT -p " $1 " -s 127.0.0.1/8 -d 127.0.0.1/8 --sport " $2 " -j ACCEPT"; \
        print "/sbin/iptables -A INPUT -p " $1 " -s 127.0.0.1/8 -d 127.0.0.1/8 --dport " $2 " -j ACCEPT"; \
-	   print ""; \
+           print ""; \
        system("/sbin/iptables -A OUTPUT -p " $1 " -s 127.0.0.1/8 -d 127.0.0.1/8 --sport " $2 " -j ACCEPT"); \
        system("/sbin/iptables -A OUTPUT -p " $1 " -s 127.0.0.1/8 -d 127.0.0.1/8 --dport " $2 " -j ACCEPT"); \
        system("/sbin/iptables -A INPUT -p " $1 " -s 127.0.0.1/8 -d 127.0.0.1/8 --sport " $2 " -j ACCEPT"); \
        system("/sbin/iptables -A INPUT -p " $1 " -s 127.0.0.1/8 -d 127.0.0.1/8 --dport " $2 " -j ACCEPT"); }' $allowed_localhost;
 
+#/sbin/iptables -A OUTPUT -s 127.0.0.1/8 -d 127.0.0.1/8 -j ACCEPT
+#/sbin/iptables -A OUTPUT -s 127.0.0.1/8 -d 127.0.0.1/8 -j ACCEPT
+#/sbin/iptables -A INPUT -s 127.0.0.1/8 -d 127.0.0.1/8 -j ACCEPT
 
+#/sbin/ip6tables -A FORWARD -s ::1 -d ::1 -j ACCEPT
+#/sbin/ip6tables -A INPUT -s ::1 -d ::1 -j ACCEPT
+#/sbin/ip6tables -A FORWARD -s ::1 -d ::1 -j ACCEPT
 
 ### lets list what is set up:
 echo "**************** IP4 TABLES:"
@@ -115,15 +138,34 @@ echo "**************** IP4 TABLES:"
 /sbin/ip6tables -P FORWARD DROP
 
 
-/sbin/ip6tables -A INPUT -p tcp -s $I6LO_ADDR -d $I6LO_ADDR -j ACCEPT
-/sbin/ip6tables -A OUTPUT -p tcp -s $I6LO_ADDR -d $I6LO_ADDR -j ACCEPT
-/sbin/ip6tables -A INPUT -p udp -s $I6LO_ADDR -d $I6LO_ADDR -j ACCEPT
-/sbin/ip6tables -A OUTPUT -p udp -s $I6LO_ADDR -d $I6LO_ADDR -j ACCEPT
+#/sbin/ip6tables -A INPUT -p tcp -s $I6LO_ADDR -d $I6LO_ADDR -j ACCEPT
+#/sbin/ip6tables -A OUTPUT -p tcp -s $I6LO_ADDR -d $I6LO_ADDR -j ACCEPT
+#/sbin/ip6tables -A INPUT -p udp -s $I6LO_ADDR -d $I6LO_ADDR -j ACCEPT
+#/sbin/ip6tables -A OUTPUT -p udp -s $I6LO_ADDR -d $I6LO_ADDR -j ACCEPT
+
+
+#####################
+# LOCALHOST ONLY
+awk '{ print "/sbin/ip6tables -A OUTPUT -p " $1 " -s ::1 -d ::1 --sport " $2 " -j ACCEPT"; \
+       print "/sbin/ip6tables -A OUTPUT -p " $1 " -s ::1 -d ::1 --dport " $2 " -j ACCEPT"; \
+       print "/sbin/ip6tables -A INPUT -p " $1 " -s ::1 -d ::1 --sport " $2 " -j ACCEPT"; \
+       print "/sbin/ip6tables -A INPUT -p " $1 " -s ::1 -d ::1 --dport " $2 " -j ACCEPT"; \
+           print ""; \
+       system("/sbin/ip6tables -A OUTPUT -p " $1 " -s ::1 -d ::1 --sport " $2 " -j ACCEPT"); \
+       system("/sbin/ip6tables -A OUTPUT -p " $1 " -s ::1 -d ::1 --dport " $2 " -j ACCEPT"); \
+       system("/sbin/ip6tables -A INPUT -p " $1 " -s ::1 -d ::1 --sport " $2 " -j ACCEPT"); \
+       system("/sbin/ip6tables -A INPUT -p " $1 " -s ::1 -d ::1 --dport " $2 " -j ACCEPT"); }' $allowed_localhost;
+
+
+#/sbin/ip6tables -A INPUT -i lo -s $UNIVERSE -d $UNIVERSE -j ACCEPT
+#/sbin/ip6tables -A INPUT -i lo -j ACCEPT
+#/sbin/ip6tables -A OUTPUT -o lo -s $UNIVERSE -d $UNIVERSE -j ACCEPT
+#/sbin/ip6tables -A OUTPUT -o lo -j ACCEPT
+
 ###############################################
 # Show what's set up:
 echo "********************* IP6 TABLES:"
 /sbin/ip6tables -L
-
 
 echo "***** restarting fail2ban to get the tables back for that"
 /etc/init.d/fail2ban restart
